1921f6fefb073a6e7260bce23d869e8380e909c6
Neptune-LiuChuansen <samuel@neptune-robotics.com>
2023-08-01 14:52:33 +0800

    start naiad_fog node

--- launch/robot_n1.launch.py
+++ launch/robot_n1.launch.py
@@ -5,16 +5,19 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='turtlesim',
-            executable='turtlesim_node',
-            namespace="turtlesim1",
-            name='node1'
+            package='naiad_chassis',
+            executable='naiad_chassis',
+            name='n1_chassis'
         ),
         Node(
-            package='turtlesim',
-            executable='turtlesim_node',
-            namespace='turtlesim2',
-            name='node2'
+            package='naiad_fog',
+            executable='naiad_fog',
+            namespace='fog_main',
+            name='n1_fog',
+            parameters=[{'serial_port' : '/dev/ttyTHS1'}, 
+                        {'serial_options' : '115200'},
+                        {'state_publish_period' : 100},
+                        {'vofa_service' : '9701:0:gx,gy,gz,ax,ay,az,er,ey,ep'}]
         )
     ])
 
