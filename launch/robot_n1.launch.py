5bff160b82294e0ccde22a277a7411c7e81b705c
neptune-robot <samule@neptune-robotics.com>
2023-08-02 22:17:55 -0400

    job stash

--- launch/robot_n1.launch.py
+++ launch/robot_n1.launch.py
@@ -7,17 +7,21 @@ def generate_launch_description():
         Node(
             package='naiad_chassis',
             executable='naiad_chassis',
-            name='n1_chassis'
+            name='n1_chassis',
+            parameters=[{'serial_port' : '/dev/ttyTHS2'}, 
+                        {'serial_options' : '460800'},
+                        {'debug_tcp_port' : 9600}
+                    ]
         ),
         Node(
             package='naiad_fog',
             executable='naiad_fog',
-            namespace='fog_main',
             name='n1_fog',
             parameters=[{'serial_port' : '/dev/ttyTHS1'}, 
                         {'serial_options' : '115200'},
                         {'state_publish_period' : 100},
-                        {'vofa_service' : '9701:0:gx,gy,gz,ax,ay,az,er,ey,ep'}]
+                        {'vofa_service' : '9701:gx,gy,gz,ax,ay,az,er,ey,ep'}
+                    ]
         )
     ])
 
